6b99774f86127420043439dc7d24ba566cb6da26
Albu Alex-Stefan <alexmhh1234@gmail.com>
2021-11-21 11:47:10 +0200

    done app

---
+++ App.vue
@@ -0,0 +1,17 @@
+<template>
+  <HomeScreen />
+</template>
+
+<script>
+import HomeScreen from "./components/HomeScreen";
+export default{
+  name: "App",
+  components:{
+    HomeScreen
+  }
+}
+</script>
+
+<style>
+
+</style>
